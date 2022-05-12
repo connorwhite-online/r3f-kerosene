2f1a6a50706c5bb5eea6298143722ce8291a6411
Connor White <connorwhite@connors-imac.lan>
2022-05-12 13:50:59 -0700

    helper-bot.gltf loaded + jsx component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { OrbitControls, Stats, PerspectiveCamera } from '@react-three/drei';
+import { Canvas } from 'react-three/fiber';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='web-gl'>
+       <Canvas>
+        <pointLight position={[10, 10, 10]} />
+      </Canvas>
     </div>
+   
   );
 }
 
