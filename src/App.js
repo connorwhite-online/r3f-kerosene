99eb9642ef251471d223fc32e50e13ea43943bed
Connor White <connorwhite@connors-imac.lan>
2022-05-12 21:44:37 -0700

    comments added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { OrbitControls, Stats, PerspectiveCamera } from '@react-three/drei';
-import { Canvas } from 'react-three/fiber';
+import { Canvas } from 'react-three-fiber';
 
+// import jsx module like so after converting your GLTF at https://gltf.pmnd.rs/
+import HelperBot from './HelperBot';
 
 function App() {
   return (
-    <div className='web-gl'>
+    <div id='webgl'>
        <Canvas>
-        <pointLight position={[10, 10, 10]} />
+         <pointLight position={[10, 10, 10]} />
+        {/* Add converted jsx components to the canvas */} 
+        <HelperBot />
+        <OrbitControls />
+        <Stats />
       </Canvas>
     </div>
    
--- src/helperBot.js
+++ src/helperBot.js
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
-export default function Model(props) {
+export default function HelperBot(props) {
   const group = useRef();
   const { nodes, materials } = useGLTF("/helper-bot.gltf");
   return (
