e627ddcb3190c9cd83f42484ed40e1bcf6302b8c
Connor White <connorwhite@connors-imac.lan>
2022-05-13 02:23:07 -0700

    simplified comments and procedural templating

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import React from 'react';
 import { OrbitControls, Stats, PerspectiveCamera } from '@react-three/drei';
 import { Canvas } from 'react-three-fiber';
 
-// import jsx module like so after converting your GLTF at https://gltf.pmnd.rs/
+// import jsx module like so after converting your GLTF at https://gltf.pmnd.rs/ and put the .gltf file in the public folder
 import HelperBot from './HelperBot';
 
 function App() {
   return (
     <div id='webgl'>
        <Canvas>
-         <pointLight position={[10, 10, 10]} />
+         <PerspectiveCamera position={[10, 10, 10]} />
+         <spotLight position={[10, 10, 10]} />
         {/* Add converted jsx components to the canvas */} 
         <HelperBot />
         {/* Add any Canvas controls here */}
-        <OrbitControls />
+        <OrbitControls enableZoom="false" />
         <Stats />
       </Canvas>
     </div>
