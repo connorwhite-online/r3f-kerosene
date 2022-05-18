2ce7bee4d75a516a0b5ad3a4f9c38daf0b326c8c
Connor White <connorwhite@connors-imac.lan>
2022-05-18 10:59:27 -0700

    zoom limitations + spotLight specs

--- src/App.js
+++ src/App.js
@@ -10,19 +10,26 @@ function App() {
   return (
     <div id='webgl'>
       <Suspense>
-       <Canvas camera={{ position: [10, 5, 5], fov: 20 }}>
-         <spotLight position={[10, 10, 1]} />
-        {/* Add converted jsx components to the canvas */} 
-        <Kerosene />
-        {/* Zoom, panning, and rotation controls for the scene */}
-        <OrbitControls 
-          autoRotate
-          autoRotateSpeed={0.5}
-          zoomSpeed={0.75} 
-        />
-        {/* Stats is a time-based log of the renderer's frame-rate */}
-        <Stats />
-      </Canvas>
+        <Canvas camera={{ position: [10, 5, 5], fov: 20 }}>
+          <spotLight 
+            intensity={1}
+            angle={0.25}
+            position={[10, 10, 1]} 
+          />
+          {/* Add converted jsx components to the canvas */} 
+          <Kerosene />
+          {/* Zoom, panning, and rotation controls for the scene */}
+          <OrbitControls 
+            autoRotate
+            autoRotateSpeed={0.5}
+            enableZoom
+            maxDistance={20}
+            minDistance={5}
+            zoomSpeed={0.75} 
+          />
+          {/* Stats is a time-based log of the renderer's frame-rate */}
+          <Stats />
+        </Canvas>
       </Suspense>
     </div>
    
