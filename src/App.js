00cb421a6c65e003f0a062b3ecc3e41aa221e901
Connor White <connorwhite@connors-imac.lan>
2022-05-17 21:43:28 -0700

    Suspense component added, helper-bot.gltf removed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { OrbitControls, Stats, PerspectiveCamera } from '@react-three/drei';
 import { Canvas } from 'react-three-fiber';
 
 // import jsx module like so after converting your GLTF at https://gltf.pmnd.rs/ and put the .gltf file in the public folder
-import HelperBot from './HelperBot';
 import Kerosene from './Kerosene';
 
 function App() {
   return (
     <div id='webgl'>
+      <Suspense>
        <Canvas>
          <PerspectiveCamera position={[10, 10, 10]} />
          <spotLight position={[10, 10, 10]} />
         {/* Add converted jsx components to the canvas */} 
-        <HelperBot />
         <Kerosene />
         {/* Add any Canvas controls here */}
         <OrbitControls enableZoom="false" />
         <Stats />
       </Canvas>
+      </Suspense>
     </div>
    
   );
